3e206aa41d9b68343ff6982e5eb8ea52c363578f
ayushgupta98 <98ayushgupta@gmail.com>
2021-09-30 02:29:32 -0600

    module 1 code with comments

--- Module-1/sqrt_approx/main.c
+++ Module-1/sqrt_approx/main.c
@@ -32,47 +32,53 @@
  * 
  */
 __asm int my_sqrt(int x){
-	push {r1-r7}
-	//Write your code here
-	cmp r0, #1
-	beq condition_1 // if x = 1, return 1
-	mov r1, r0 					// Load R1 with the value of x
-	ldr r2, =0xffffffff // c
-	movs r3, #0 		// done
-	movs r4, #0 		// c_old
-	movs r6, #0 		// a
-	mov r7, r0 			// b
-done_check
-	CMP r3, #1 			// if(done == 1)
-	beq end_func 		// if equal, return
-	b check_1 			// move to the second check condition
-check_1
-	CMP r2, r4 			// if c == c_old
-	beq end_func 		// if equal, return
-	b code 					// move to code execution
-code
-	mov r4, r2      // c_old = c
-	adds r5,r6,r7   // temp = a+b
-	lsrs r2,r5,#1   // c = temp/2
-	movs r5, #1
-	muls r5, r2, r5 // 
-	muls r5, r2, r5 // c*c
-	cmps r5, r0     // if c == r0
-	beq end_func    // if equal, return
-	blt set_a       // if less than, a = c
-	mov r7, r2      // b = c
-	b done_check    // loop again
-set_a
-	mov r6, r2      // a = c
-	b done_check    // loop again
+		//Write your code here
+		push{r1-r7}	//push R1-R7 registers on the stack
+		movs r2, #0 // done=0
+		movs r3, #0 // a=0
+		movs r4, r0 // b = x
+		movs r5, #0 //c_old = 0
+		ldr r6, = 0xffffffff //c = -1
+		b COND_1 //BRANCH to LABLE COND_1
 
-condition_1
-		movs r2, #1
-		b end_func
-end_func
-		mov r0, r2    // return value = c
-		pop {r1-r7}
-		bx lr         // return
+COND_1
+	cmp r7, #0 //COMPARE done with 0
+	bne COND_2 //if done != 0 then BRANCH to COND_2
+	beq END_FUN //if done == 0 then BRANCH to END_FUN
+	
+COND_2
+	CMP r5, r6 //compare c_old and c
+	bne LOOP_BODY //c_old != c then BRANCH to LOOP_BODY
+	beq END_FUN //c_old == c then BRANCH to END_FUN
+	
+IF_BODY
+	movs r2, #1 //done = 1
+	b COND_1 //BRANCH to COND_1
+
+ELSE_IF_BODY 
+	movs r3, r6 //a = c
+	b COND_1 //BRANCH to COND_1 
+	
+ELSE_BODY
+	movs r4, r6 //b = c
+	b COND_1 //BRANCH to COND_1
+
+LOOP_BODY
+	movs r5, r6 //c_old = c
+	adds r7, r3, r4 //tmp = a+b
+	lsrs r6, r7, #1 // c = tmp>>1
+	movs r7, #1; // tmp = 1
+	muls r7, r6, r7 //tmp = c*1 
+	muls r7, r6, r7 //tmp = c*c
+	cmp r7, r0 //tmp == x
+	beq IF_BODY // if tmp == c then BRANCH to IF_BODY
+	blt ELSE_IF_BODY // if tmp < c then BRANCH to ELSE_IF_BODY
+	bgt ELSE_BODY // if tmp > c then BRANCH to ELSE_BODY
+	
+END_FUN
+	movs r0, r6 //move value of c in r6
+	pop{r1-r7} //pop r1-r7 from the stack
+	bx lr //branch to the Link Register
 }
 
 /*----------------------------------------------------------------------------
@@ -85,18 +91,16 @@ end_func
  */
 int main(void){
 	volatile int r, j=0;
-	int i;                  
+	int i;
+	             
   r = my_sqrt(0);     // should be 0
   r = my_sqrt(25);    // should be 5
 	r = my_sqrt(133); 	// should be 11
-	r = my_sqrt(2); 	  // should be 1
-	r = my_sqrt(4); 	  // should be 2
-	r = my_sqrt(22); 	  // should be 4
-	r = my_sqrt(121); 	// should be 11
   for (i=0; i<10000; i++){
 		r = my_sqrt(i);
     j+=r;
   }
+
 	while(1)
 		;
 }
